b1215eb447567bc16f07ea5265fe7ceffe66c275
mkrkppl <endang.hidayat@mail.unpas.ac.id>
2013-05-03 14:08:24 +0700

    create new project

---
+++ Project_version/src/ac/id/unpas/rkppl/Test.java
@@ -0,0 +1,14 @@
+package ac.id.unpas.rkppl;
+
+public class Test {
+
+	public static void test() {
+		System.out.println("Hello World");
+	}
+	
+	
+	public static void main(String[] args) {
+		test();
+	}
+
+}
